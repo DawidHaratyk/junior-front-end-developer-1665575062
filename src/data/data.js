27fc0dfe425c4ef47d44e60fe9e14ff30398484a
Dawid Haratyk <dharatyk@onet.pl>
2022-10-13 11:16:19 +0200

    feat: add mocked tasksData

---
+++ src/data/data.js
@@ -0,0 +1,176 @@
+import completedTask from "../assets/completedTask";
+import activeTask from "../assets/activeTask";
+import unlockedTask from "../assets/unlockedTask";
+
+export const tasksData = [
+  {
+    title: "Application Setup",
+    status: completedTask,
+    businessContexts: [
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Kirsten Aniston",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+      {
+        title: "New sprint, tasks and intro information.",
+        author: "Olga Nelson",
+        date: "2022-09-20",
+        content: [
+          "Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...",
+        ],
+      },
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Olga Nelson",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Static Books List",
+    status: completedTask,
+    businessContexts: [
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Kirsten Aniston",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+      {
+        title: "New sprint, tasks and intro information.",
+        author: "Olga Nelson",
+        date: "2022-09-20",
+        content: [
+          "Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...",
+        ],
+      },
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Olga Nelson",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Administration Panel",
+    status: completedTask,
+    businessContexts: [
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Kirsten Aniston",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+      {
+        title: "New sprint, tasks and intro information.",
+        author: "Olga Nelson",
+        date: "2022-09-20",
+        content: [
+          "Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...",
+        ],
+      },
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Olga Nelson",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Connect Admin with Frontend",
+    status: activeTask,
+    businessContexts: [
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Kirsten Aniston",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+      {
+        title: "New sprint, tasks and intro information.",
+        author: "Olga Nelson",
+        date: "2022-09-20",
+        content: [
+          "Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...",
+        ],
+      },
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Olga Nelson",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+    ],
+  },
+  {
+    title: "Book Review Feature",
+    status: unlockedTask,
+    businessContexts: [
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Kirsten Aniston",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+      {
+        title: "New sprint, tasks and intro information.",
+        author: "Olga Nelson",
+        date: "2022-09-20",
+        content: [
+          "Hi Eric, congratulations on completing the previous assignment. This time you will have to focus on...",
+        ],
+      },
+      {
+        title: "Application has been accepted  🎉  🙌",
+        author: "Olga Nelson",
+        date: "2022-08-15",
+        content: [
+          "Hello! My name is Kirsten, and I'm super happy to announce that your application to join Coders Family has been accepted! 🎉 🙌",
+          "You really impressed us during the interview process, and we'd like to offer you a project with {COMPANY}. We've been working with {COMPANY} for quite some time, and it's important for us to keep them satisfied with our services — hence why we believe you'll be a great fit for this job.",
+          "Here's the project overview: You'll be responsible for building a database and page for recording and displaying book ratings. We'd like to work in two week-long sprints, we've already given you a head start and prepared your tasks to deliver in your first sprint.",
+        ],
+      },
+    ],
+  },
+];
