6409d9849594f9aa47923bfa6335796856e0ff1f
Dawid Haratyk <dharatyk@onet.pl>
2022-10-14 19:07:09 +0200

    fix: remove unnecessary file

--- src/studia.js
+++
@@ -1,9 +0,0 @@
-let wiek = 21
-
-const country = 'usa'
-
-if (country === 'poland' && wiek >= 18) {
-  console.log('moze sprzedawac w polsce')
-} else if (country === 'usa' && wiek >= 21) {
-  console.log('moze sprzedawac w usa')
-}
